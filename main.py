ec646a371ad14ef8c76f5a35bc646855e63e14f6
Kumupun <nastynski@gmail.com>
2025-09-30 22:02:49 +0300

    NEWTON WORKING YIPPPIE

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ def print_menu():
     print("2. Find root using Simple Iteration method")
     print("3. Exit")
 
-def Newton():
-    x0 = float(input("Enter initial guess x0: "))
-    epsilon = float(input("Enter epsilon: "))
-    max_iter = int(input("Enter max iterations: "))
+def Newton(inter,x0, epsilon):
+    (l,r) = map(float, inter.split())
+    max_iter = newton.approx(x0, l, r, epsilon)
+    if max_iter is None:
+        return [None, None]
     res = newton.newton(x0,epsilon,max_iter)
     res_s = opt.newton(lambda x: x**3 + x**2 - 4*x - 4, x0)
     return [res, res_s]
 
 
 def Simple_iteration():
+    
     x0 = float(input("Enter initial guess x0: "))
     epsilon = float(input("Enter epsilon: "))
     max_iter = int(input("Enter max iterations: "))
@@ -28,14 +30,17 @@ def main():
     c= input("Choose an option (1-3): ")
     if c == '1':
         print("Newton's Method Selected.")
-        result = Newton()
+        inter = input("Enter interval [l, r]: ")
+        x0 = float(input("Enter initial guess x0: "))
+        epsilon = float(input("Enter epsilon: "))
+        result = Newton(inter,x0, epsilon)
         answ = result[0]
         scpy_res = result[1]
         if answ is not None:
             print("Root found:", answ)
         else:
             print("No root found.")
-        print("Scipy result:", scpy_res)
+            print("Scipy result:", scpy_res)
 
     elif c == '2':
         print("Simple Iteration Method Selected.")
--- newton.py
+++ newton.py
@@ -1,3 +1,4 @@
+import math
 def newton(x0,epsilon,max_iter):
     f = lambda x: x**3+x**2-4*x-4
     Df = lambda x: 3*x**2+2*x-4
@@ -18,3 +19,23 @@ def newton(x0,epsilon,max_iter):
     print('Exceeded maximum iterations. No solution found.')
     return None
 
+def approx (x0,lend, rend, epsilon):
+    f = lambda x: x**3+x**2-4*x-4
+    Df = lambda x: 3*x**2+2*x-4
+    D2f = lambda x: 6*x+2
+    l=max(abs(x0-lend), abs(x0-rend))
+    if f(lend)*f(rend) >= 0:
+        print("The function must have different signs at the endpoints.")
+        return None
+    elif f(x0)*D2f(x0) >= 0:
+        print("The initial approximation must satisfy the condition f(x0)*f''(x0) < 0.")
+        return None
+    m1 = min(Df(lend), Df(rend))
+    M2 = max(D2f(lend), D2f(rend))
+    q=M2*l/(2*m1)
+    if q >= 1:
+        print("The method may not converge q =", q)
+        return None
+    n= int(math.log2((math.log(l/epsilon)/math.log(1/q))+1)+1)
+    print("The number of iterations apriori:", n)
+    return n
